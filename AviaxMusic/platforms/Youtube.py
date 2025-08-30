498923d7f61cdb4c14642f57c89b081440344397
shadowheroku <shadowheroku@gmail.com>
2025-08-30 18:14:24 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from Powers.utils.database import is_on_off
-from Powers.utils.formatters import time_to_seconds
+from AviaxMusic.utils.database import is_on_off
+from AviaxMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
